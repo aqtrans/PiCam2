31c909f7029f416904d11037f5bc85411bca5078
Vincent Swarte <vincent@vinsert.nl>
2015-05-02 23:10:06 +0200

    Fixed problem with the hostname, you can now specify a port too

--- server.go
+++ server.go
@@ -2,13 +2,19 @@ package main
 
 import (
   "fmt"
+  "flag"
   "time"
   "net/http"
+  "os/exec"
   "github.com/gorilla/websocket"
 )
 
 func main(){
-  log( "Started server")
+  port := flag.String("p", "3232", "specifies the port you want to be hosting on")
+
+  flag.Parse()
+
+  log( "Started server on port " + *port )
 
   upgrade := websocket.Upgrader{
     ReadBufferSize: 1024,
@@ -16,41 +22,39 @@ func main(){
   }
 
   http.HandleFunc("/websocket", func( res http.ResponseWriter, req *http.Request){
-    handleConnection( res, req, upgrade )
+   handleConnection( res, req, upgrade )
   })
 
   http.Handle("/", http.FileServer(http.Dir("public/")))
 
-  err := http.ListenAndServe(":8080", nil)
+  err := http.ListenAndServe(":" + *port, nil)
   if err != nil {
     panic( err )
   }
 }
 
 func handleConnection( res http.ResponseWriter, req *http.Request, upgrade websocket.Upgrader ){
+  log("Started a stream!")
+
   conn, err := upgrade.Upgrade(res, req, nil)
   if err != nil {
     panic( err )
   }
 
   for{
-    messageType, message, err := conn.ReadMessage()
-    if err != nil {
-      panic( err )
+    imageData, err := exec.Command("fswebcam", "-").Output()
+    if err != nil{
+      panic(err)
     }
 
-    messageStr := string(message[:])
-    log( messageStr )
+    // Throttle this shit
+    time.Sleep( time.Millisecond * 200 )
 
-    if( messageStr == "ping" || messageStr == "PING" ){
-      conn.WriteMessage( messageType, []byte("PONG") )
-    }
-
-    // Send the message to your business logic
+    conn.WriteMessage( websocket.BinaryMessage, imageData )
   }
+
 }
 
-// Should probably shove this into a library
 func log( message string ){
   fmt.Printf( "[%2d:%2d] : %s \n", time.Now().Hour(), time.Now().Minute(), message )
 }
